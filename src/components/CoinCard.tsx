74f89e1480166c8e110b8e3b53450a109217817e
starlash7 <yang92sw@gmail.com>
2024-08-16 10:38:31 +0900

    image 가져오기

--- src/components/CoinCard.tsx
+++ src/components/CoinCard.tsx
@@ -1,4 +1,5 @@
-import { Flex } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/next-js";
+import { Flex, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
 interface CoinCardProps {
@@ -6,7 +7,13 @@ interface CoinCardProps {
 }
 
 const CoinCard: FC<CoinCardProps> = ({ coin }) => {
-    return <Flex>{coin.name}</Flex>;
+    return (
+        <Flex>
+            <Text>{coin.market_cap_rank}</Text>
+            <Image src={coin.image} alt={coin.name} width={8} height={8} />
+            <Text>{coin.name}</Text>
+        </Flex>
+    );
 };
 
 export default CoinCard;
